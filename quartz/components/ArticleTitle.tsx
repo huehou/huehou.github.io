eed7b512ff4f6c24712dd77ac55623523f54811b
Alexander_Hue <Alexander_hue1993@hotmail.com>
2023-10-11 00:15:10 +0800

    Added author and reference to book and paper, respectively

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -2,25 +2,42 @@ import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 function ArticleTitle({ fileData }: QuartzComponentProps) {
   const title = fileData.frontmatter?.title
-  const reference = fileData.frontmatter?.reference
   const author = fileData.frontmatter?.author
+  const reference = fileData.frontmatter?.reference
   if (title) {
     if (author) {
-      return <h1>{title}</h1><br><h6>by {author}</h6>
-    } else if (reference) {
-      return <h1>{title}</h1><br><h6>Reference: {reference}</h6>
-    } else {
-      //return <h1 class="article-title">{title}</h1>
-      return <h1>{title}</h1>
-    }
+	  return (
+	    <div>
+			<h1>
+			  {title}
+			</h1>
+			<h6>
+			  by {author}
+			</h6>
+		</div>
+	  )
+	} else if (reference) {
+	  return (
+	    <div>
+			<h1>
+			  {title}
+			</h1>
+			<h6>
+			  Reference: {reference}
+			</h6>
+		</div>
+	  )
+	} else {
+	  return <h1>{title}</h1>
+	}
   } else {
     return null
   }
 }
-//ArticleTitle.css = `
-//.article-title {
-  //margin: 2rem 0 0 0;
-//}
+ArticleTitle.css = `
+.article-title {
+  margin: 2rem 0 0 0;
+}
 `
 
 export default (() => ArticleTitle) satisfies QuartzComponentConstructor
