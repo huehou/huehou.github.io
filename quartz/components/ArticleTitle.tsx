8838f7a926cb93da33e0e0cfe7b0c54196d3dc04
huehou <Alexander_hue1993@hotmail.com>
2023-10-10 23:26:07 +0800

    Trying to put author and reference to book and paper, respectively

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -2,16 +2,25 @@ import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 function ArticleTitle({ fileData }: QuartzComponentProps) {
   const title = fileData.frontmatter?.title
+  const reference = fileData.frontmatter?.reference
+  const author = fileData.frontmatter?.author
   if (title) {
-    return <h1 class="article-title">{title}</h1>
+    if (author) {
+      return <h1>{title}</h1><br><h6>by {author}</h6>
+    } else if (reference) {
+      return <h1>{title}</h1><br><h6>Reference: {reference}</h6>
+    } else {
+      //return <h1 class="article-title">{title}</h1>
+      return <h1>{title}</h1>
+    }
   } else {
     return null
   }
 }
-ArticleTitle.css = `
-.article-title {
-  margin: 2rem 0 0 0;
-}
+//ArticleTitle.css = `
+//.article-title {
+  //margin: 2rem 0 0 0;
+//}
 `
 
 export default (() => ArticleTitle) satisfies QuartzComponentConstructor
